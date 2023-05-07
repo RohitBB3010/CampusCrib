2dbb5a8c0bc355332eadad9a7cd6d1fc6df39200
Rohit Bhandwalkar <108782412+RohitBB3010@users.noreply.github.com>
2023-05-08 00:54:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function change1(){
+    document.getElementById("tosignup").style.display = "none";
+    document.getElementById("signedup").style.display = "block";
+    document.getElementById("purpose").style.display = "none";
+    document.getElementById("signup").style.backgroundColor = "#357266";
+    document.getElementById("signin").style.backgroundColor = "#C5C392";
+    document.getElementById("signup").style.color = "#ffffff";
+}
+
+function change2(){
+    document.getElementById("signedup").style.display = "none";
+    document.getElementById("tosignup").style.display = "block";
+    document.getElementById("purpose").style.display = "none";
+    document.getElementById("signup").style.backgroundColor = "#C5C392";
+    document.getElementById("signin").style.backgroundColor = "#357266";
+    document.getElementById("signup").style.color = "#ffffff";
+}
+
+
+
